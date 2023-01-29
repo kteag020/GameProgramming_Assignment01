b1121cd55bdc7aad2a7a05462b670b06e3e65b9c
kteag020 <123774251+kteag020@users.noreply.github.com>
2023-01-29 03:46:09 -0600

    Add files via upload

---
+++ Assets/Scripts/Text.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextChange : MonoBehaviour
+{
+
+    public void Change(float f)
+    {
+        GetComponent<Text>().text = f.ToString();
+    }
+
+}
